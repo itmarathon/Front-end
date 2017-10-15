8355eaec29db5a50595f9b7bbd9e55dc934fc9b4
Vlad <laninskiy@gmail.com>
2017-10-15 14:25:41 +0300

    add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Footer from './Components/Footer'
 
 class App extends Component {
   render() {
     return (
+      <div>
       <div className="App">
         <p>Hello, World!</p>
+        <div id="indent"></div>
+      </div>
+      <Footer />
       </div>
     );
   }
