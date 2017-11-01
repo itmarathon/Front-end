8240f52df4e91697f00d334ec5b95d3a60c676b7
hakalo <sergey.khakalo@gmail.com>
2017-11-01 19:07:48 +0300

    fixed header(added some tamoto,need  more fixes, changed some colors and style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Menu, Button } from 'semantic-ui-react';
 import './Header.css';
 
 
-const Header = () => (
-  <div className="wrapHeader">
-    <div className="logo"><Link to="/">IT-Marathon</Link></div>
-    <div className="sign">
-      <Link to="/sign-up">Регистрация /</Link>
-      <Link to="/sign-in">Вход</Link>
-    </div>
-  </div>
-);
+class Header extends React.Component {
+  state = {};
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <Menu className="mainHeader" borderless>
+        <Menu.Item
+          className="logoTop"
+          position="left"
+          name="logo"
+          active={activeItem === 'logo'}
+          onClick={this.handleItemClick}
+        >
+          <Link to="/">IT-Marathon</Link>
+        </Menu.Item>
+        <Menu position="right">
+          <Menu.Item
+            name="signUp"
+            active={activeItem === 'signUp'}
+            onClick={this.handleItemClick}
+            compact
+          >
+            <Link to="/sign-up">Регистрация</Link>
+          </Menu.Item>
+          <Menu.Item
+            name="signIn"
+            active={activeItem === 'signIn'}
+            onClick={this.handleItemClick}
+          >
+            <Button><Link to="/sign-in">Войти</Link></Button>
+          </Menu.Item>
+        </Menu>
+      </Menu>
+    );
+  }
+}
 
 export default Header;
