d54cb64c50bf0e66aa1fc17a7b07bad067d152c6
Vlad <laninskiy@gmail.com>
2017-11-12 18:33:44 +0300

    Add getCities api method

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,1 +1,19 @@
-const apiUrl = 'http://localhost:3000/';
+const apiUrl = 'http://localhost:3000';
+
+const getCities = async () => {
+  let response;
+
+  try {
+    response = await fetch(`${apiUrl}/api/list/city`);
+  } catch (e) {
+    console.error(e);
+  }
+
+  const { data } = await response.json();
+  console.log(data);
+  return data.cities;
+};
+
+export default {
+  getCities,
+};
