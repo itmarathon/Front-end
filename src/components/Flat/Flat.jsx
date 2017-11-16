f9a826061702285088f5889b144b7e219d524de4
Vlad <laninskiy@gmail.com>
2017-11-16 23:51:55 +0300

    Fix Flat view

--- src/components/Flat/Flat.jsx
+++ src/components/Flat/Flat.jsx
@@ -7,22 +7,29 @@ const Flat = ({
   city,
   rooms,
   price,
+  type,
 }) => (
   <div className="Flat">
     <img src={img} alt={title} />
     <p>{title}</p>
-    <p>{city}</p>
-    <p>{rooms}</p>
-    <p>{price}</p>
+    <p>Город: {city}</p>
+    <p>Комнат: {rooms}</p>
+    <p>Цена: {price}</p>
+    <p>Тип: {type}</p>
   </div>
 );
 
 Flat.propTypes = {
-  img: PropTypes.string.isRequired,
+  img: PropTypes.string,
   title: PropTypes.string.isRequired,
   city: PropTypes.string.isRequired,
   rooms: PropTypes.string.isRequired,
   price: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
 };
+
+Flat.defaultProps = {
+  img: '/tmp.jpg',
+}
 
 export default Flat;
